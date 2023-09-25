e7ce4d0a89f5d3c44c5d0d5abe1bf584aa458fb2
username <114908192+HaNgocLanAnh@users.noreply.github.com>
2023-09-25 21:25:04 +0700

    25/9/23

--- Lesson09/JS/ex09.js
+++ Lesson09/JS/ex09.js
@@ -79,5 +79,31 @@ var day = new Date().getDay();
             console.log("Hôm nay là: Thứ bảy");
             break;
     }
-//
+// Đếm số ngày trong tháng
+var month = 2;
+var day_year = 365;
+    switch(month){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            console.log("Tháng " + month + " có 31 ngày");
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            console.log("Tháng " + month + " có 30 ngày");
+            break;
+        case 2:
+            if(day_year % 2 == 0){
+                console.log("Tháng " + month + " có 29 ngày");
+            } else{
+                console.log("Tháng " + month + " có 28 ngày");
+            }
+            break;
+    }
 
